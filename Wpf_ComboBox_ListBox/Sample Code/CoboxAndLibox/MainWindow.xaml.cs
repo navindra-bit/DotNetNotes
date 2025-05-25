2de8dcac1c077844a629fa5671ef5f9e48f49eba
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-05-25 22:18:18 +0530

    ComboBox_ListBox Sample code update1

---
+++ Wpf_ComboBox_ListBox/Sample Code/CoboxAndLibox/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoboxAndLibox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            object newitem = newtextadd.Text;
+            combo.Items.Add(newitem);
+            newtextadd.Text = "";
+        }
+
+        private void c_Click(object sender, RoutedEventArgs e)
+        {
+            int count = combo.Items.Count;
+            //combo.Items.Clear();
+           // combo.Items.Remove("India");
+            combo.Items.RemoveAt(count-1);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int count1 = lb.SelectedItems.Count;
+
+            for (int i = 0; i < count1; i++)
+            {
+                ListBoxItem listBoxItem = (ListBoxItem)lb.SelectedItems[i];
+                if(listBoxItem.Content is not null)
+                {
+                MessageBox.Show(listBoxItem.Content.ToString());
+                }
+            }
+        }
+    }
+}
